406ea23a71f7141801d552749528e50f3672d479
Arifin Firdaus <arifin.firdaus@icehousecorp.com>
2021-10-29 15:02:18 +0700

    Initial Commit

---
+++ AppStoreTodayApp/AppStoreTodayAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreTodayAppApp.swift
+//  AppStoreTodayApp
+//
+//  Created by Arifin Firdaus on 29/10/21.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreTodayAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
